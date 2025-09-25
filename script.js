1d0f350a3dae8e9300d0ae3cb96fab006cb068a4
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-09-25 12:44:25 +0600

    add 2nd example

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 // =========== Example 01  |=================== 
-const student = {
-name :  "Mohi" ,
-marks :454,
-printMarks:function(){
-    console.log("marks = ", this.marks);
+// const student = {
+// name :  "Mohi" ,
+// marks :454,
+// printMarks:function(){
+//     console.log("marks = ", this.marks);
     
-},
+// },
+// };
+
+// ===========| Example 02  |=================== 
+const employee = {
+    calTax(){
+        console.log("Tax is 10%");
+        
+    },
 };
+
+const Kiron = { 
+    salary : 1000,
+}
+Argun.__proto__ = employee; 
