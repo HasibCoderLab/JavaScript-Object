3cc3df162cf6da8e4b7a14b51cbb5336a6aab0e9
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-09-25 12:51:08 +0600

    update

--- script.js
+++ script.js
@@ -17,6 +17,27 @@ const employee = {
 };
 
 const Kiron = { 
-    salary : 1000,
+    salary : 1111,
+    calTax(){
+        console.log("Tax is 25%");
+        
+    }
+};
+const Kiron2 = { 
+    salary : 2222,
+};
+const Kiron3 = { 
+    salary : 3333,
 }
-Argun.__proto__ = employee; 
+const Kiron4 = { 
+    salary : 4444,
+};
+const Kiron5 = { 
+    salary : 5555,
+};
+
+Kiron.__proto__ = employee; 
+Kiron2.__proto__ = employee; 
+Kiron3.__proto__ = employee; 
+Kiron4.__proto__ = employee; 
+Kiron5.__proto__ = employee; 
